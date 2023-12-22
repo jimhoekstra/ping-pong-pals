a3c3c57b199e3e8b81b1745fec81a59208273625
Jim <jimhoekstra2@gmail.com>
2023-12-22 19:47:57 +0100

    bug fixes and add test

--- scoreboard/tests.py
+++ scoreboard/tests.py
@@ -13,18 +13,26 @@ class TestPages(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
+    def test_unauthenticated_leagues_page(self):
+        '''
+        Test that leagues page returns a redirect to the login page if not logged in.
+        '''
+        response = self.client.get('/leagues', follow=True)
+        self.assertEqual(response.redirect_chain[0], ('/accounts/login?next=/leagues', 302))
+        self.assertEqual(response.status_code, 200)
+    
     def test_unauthenticated_games_page(self):
         '''
         Test that games page returns a redirect to the login page if not logged in.
         '''
-        response = self.client.get('/games')
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, '/accounts/login?next=/games')
+        response = self.client.get('/games', follow=True)
+        self.assertEqual(response.redirect_chain[0], ('/accounts/login?next=/games', 302))
+        self.assertEqual(response.status_code, 200)
 
     def test_unauthenticated_players_page(self):
         '''
         Test that players page returns a redirect to the login page if not logged in.
         '''
-        response = self.client.get('/players')
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, '/accounts/login?next=/players')
+        response = self.client.get('/players', follow=True)
+        self.assertEqual(response.redirect_chain[0], ('/accounts/login?next=/players', 302))
+        self.assertEqual(response.status_code, 200)
